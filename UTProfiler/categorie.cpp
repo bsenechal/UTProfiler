80b11a23d420318fe3099f9b198b6f3b190d8fa7
bsenechal <b_e_n@live.fr>
2014-06-10 11:29:11 +0200

    Ajout de commentaires

--- UTProfiler/categorie.cpp
+++ UTProfiler/categorie.cpp
@@ -1,5 +1,13 @@
+/**
+ * \file categorie.cpp
+ */
+
 #include "categorie.h"
 
+/**
+ * \fn Categorie::Categorie()
+ * \brief Construit une QStringList à partir des noms de chaque catégorie d'UV
+ */
 Categorie::Categorie()
 {
     dbmanager *db = dbmanager::getInstance();
--- UTProfiler/categorie.h
+++ UTProfiler/categorie.h
@@ -1,3 +1,7 @@
+/**
+ * \file categorie.h
+ */
+
 #ifndef CATEGORIE_H
 #define CATEGORIE_H
 
@@ -9,11 +13,41 @@ class Categorie : public Singleton<Categorie>
 {
     friend class Singleton<Categorie>;
     QStringList liste_categories;
+    /**
+     * \fn Categorie()
+     * \brief Construit une QStringList à partir des noms de chaque catégorie d'UV
+     */
     Categorie();
 public:
+    /**
+     * \fn getListe_categories()
+     * \brief Retourne la liste des categories d'UV
+     * \return QStringList&
+     */
     QStringList& getListe_categories() { return this->liste_categories; }
+
+    /**
+     * \fn void push_back(QString item)
+     * \brief Ajoute un élément à la fin de la liste d'UV
+     * \param QString item
+     * \return void
+     */
     void push_back(QString item) { this->liste_categories.push_back(item); }
+
+    /**
+     * \fn void removeAt(int row)
+     * \param int row
+     * \brief Supprime l'élément de la liste d'UV présent à la ligne passé en paramètre
+     * \return void
+     */
     void removeAt(int row) { this->liste_categories.removeAt(row); }
+
+    /**
+     * \fn void setListe_categories(QStringList& l)
+     * \param QStringList& l
+     * \brief Setter pour la liste de catégories d'UV
+     * \return void
+     */
     void setListe_categories(QStringList& l) { this->liste_categories = l; }
 };
 
