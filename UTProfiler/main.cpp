158617fa4edbebc83eb5da349d26b41be91dff96
bsenechal <b_e_n@live.fr>
2014-06-13 15:14:57 +0200

    connexion + titres

--- UTProfiler/main.cpp
+++ UTProfiler/main.cpp
@@ -25,8 +25,8 @@ int main(int argc, char* argv[]){
     /*! Initialisation de la BDD */
     dbmanager *db = dbmanager::getInstance();
 
-    //db->dbinitialise("QSQLITE","C:/Users/Benoît/Documents/workspace/UTProfiler/BDD/UTProfiler");
-    db->dbinitialise("QSQLITE","/Users/David/Desktop/UTC/LO21/Projet/UTProfilergit/UTProfiler/BDD/UTProfiler");
+    db->dbinitialise("QSQLITE","C:/Users/Benoît/Documents/workspace/UTProfiler/BDD/UTProfiler");
+    //db->dbinitialise("QSQLITE","/Users/David/Desktop/UTC/LO21/Projet/UTProfilergit/UTProfiler/BDD/UTProfiler");
 
     Accueil acc;
     acc.show();
